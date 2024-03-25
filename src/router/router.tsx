ceedf50a95b7152fdc9e35ba045d1d131f759c1d
sorrel012 <wain1719@gmail.com>
2024-03-25 17:49:47 +0900

    [24.03.25] 초기 프로젝트 구축

---
+++ src/router/router.tsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import Home from '../pages/Home';
+import Profile from '../pages/Profile';
+import Skills from '../pages/Skills';
+import Projects from '../pages/Projects';
+import Contact from '../pages/Contact';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+      {
+        path: 'skills',
+        element: <Skills />,
+      },
+      {
+        path: 'projects',
+        element: <Projects />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
