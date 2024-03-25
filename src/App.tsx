74e8c2cbf33c9038a3a13a13d8a015df471ce98f
sorrel012 <wain1719@gmail.com>
2024-03-25 23:41:22 +0900

    [24.03.25] 메인 페이지 마크업(임시)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from './components/Header.tsx';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useMatch } from 'react-router-dom';
 
 function App() {
+  const mainMatch = useMatch('/');
+
   return (
     <>
-      <Header></Header>
+      {!mainMatch && <Header />}
       <Outlet />
     </>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,35 @@
+import styled from 'styled-components';
+
+const Login = styled.button``;
+
+const Title = styled.h1``;
+
+const CategoryWrapper = styled.div``;
+
+const Category = styled.button``;
+
+const Butterfly = styled.div``;
+
+const Grass = styled.div``;
+
+const Flower = styled.div``;
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Login />
+      <Title>Hyowon's Portfolio</Title>
+      <Butterfly />
+      <CategoryWrapper>
+        <Category>Profile</Category>
+        <Category>Skills</Category>
+        <Category>Projects</Category>
+        <Category>Contact</Category>
+      </CategoryWrapper>
+      <Grass />
+      <Flower />
+    </>
+  );
 }
 
 export default Home;
