60d3e6848267124d23bd338a6317eb903976aee4
sorrel012 <wain1719@gmail.com>
2024-03-25 23:41:10 +0900

    [24.03.25] 전역 스타일 설정

---
+++ src/assets/css/styled.d.ts
@@ -0,0 +1,49 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    home: {
+      bgColor: string;
+      titleColor: string;
+      textColor: string;
+    };
+    header: {
+      white: string;
+      blue: string;
+      green: string;
+      pink: string;
+    };
+    profile: {
+      bgColor: string;
+      titleColor: string;
+      labelColor: string;
+    };
+    skills: {
+      bgColor: string;
+      textColor: string;
+    };
+    projects: {
+      headerBgColor: string;
+      headerTextColor: string;
+      bgColor: string;
+      wrapperBgColor: string;
+      contentBgColor: string;
+    };
+    contact: {
+      bgColor: string;
+      headerColor: string;
+      textColor: string;
+    };
+    admin: {
+      loginColor: string;
+      navBgColor: string;
+      navBorderColor: string;
+      navTextColor: string;
+      navTextHoverColor: string;
+      textColor: string;
+      bgColor: string;
+      wrapperBgColor: string;
+      wrapperBorderColor: string;
+    };
+  }
+}
---
+++ src/assets/css/theme.ts
@@ -0,0 +1,47 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  home: {
+    bgColor: `linear-gradient(180deg, rgba(255, 249, 237, 0.86) 0%, rgba(255, 223, 225, 0.82) 51%, rgba(255, 237, 223, 0.82) 100%)`,
+    titleColor: '#FFFFFF',
+    textColor: '#6D422A',
+  },
+  header: {
+    white: '#FFFFFF',
+    blue: '#75A1CA',
+    green: '#6C8F5C',
+    pink: '#EB837F',
+  },
+  profile: {
+    bgColor: `linear-gradient(180deg, #B0E0E6 0%, #87CEEB 17%, #1E90FF 70.5%, #0077BE 99%)`,
+    titleColor: '#FFFFFF',
+    labelColor: '#880000',
+  },
+  skills: {
+    bgColor: `linear-gradient(180deg, #E9F9FD 0%, #D4F9FC 71%, #FFDAAE 100%)`,
+    textColor: '#FFFFFF',
+  },
+  projects: {
+    headerBgColor: '#FFFFFF',
+    headerTextColor: '#AE0000',
+    bgColor: '#E9EDC9',
+    wrapperBgColor: '#D4A373',
+    contentBgColor: '#FAEDCD',
+  },
+  contact: {
+    bgColor: `linear-gradient(180deg, #03255F 0%, #072B69 48%, #001231 100%)`,
+    headerColor: '#FFFFFF',
+    textColor: '#001663',
+  },
+  admin: {
+    loginColor: '#6D422A',
+    navBgColor: '#F8EDEB',
+    navBorderColor: '#D39090',
+    navTextColor: '#AF6C63',
+    navTextHoverColor: '#EB837F',
+    textColor: '#8A5B30',
+    bgColor: '#F5F5F5',
+    wrapperBgColor: '#FFFFFF',
+    wrapperBorderColor: '#E3D5CA',
+  },
+};
