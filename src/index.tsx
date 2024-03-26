03e4faefb7138d2a46ca8875ebef01da41c14ea7
sorrel012 <wain1719@gmail.com>
2024-03-26 11:05:12 +0900

    [24.03.26] 공통 css 정리

--- src/index.tsx
+++ src/index.tsx
@@ -2,98 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { theme } from './assets/css/theme.ts';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './assets/css/common.css';
-
-export const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'Fairytale_ddobak';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/naverfont_03@1.0/Fairytale_ddobak.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Pretendard-Regular';
-    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-    font-weight: 400;
-    font-style: normal;
-  }
-
-  html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-  a, abbr, acronym, address, big, cite, code,
-  del, dfn, em, img, ins, kbd, q, s, samp,
-  small, strike, strong, sub, sup, tt, var,
-  b, u, i, center,
-  dl, dt, dd, menu, ol, ul, li,
-  fieldset, form, label, legend,
-  table, caption, tbody, tfoot, thead, tr, th, td,
-  article, aside, canvas, details, embed,
-  figure, figcaption, footer, header, hgroup,
-  main, menu, nav, output, ruby, section, summary,
-  time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font: inherit;
-    vertical-align: baseline;
-    user-select: none;
-  }
-
-  /* HTML5 display-role reset for older browsers */
-  article, aside, details, figcaption, figure,
-  footer, header, hgroup, main, menu, nav, section {
-    display: block;
-  }
-
-  /* HTML5 hidden-attribute fix for newer browsers */
-  *[hidden] {
-    display: none;
-  }
-
-  body {
-    line-height: 1;
-  }
-
-  menu, ol, ul {
-    list-style: none;
-  }
-
-  blockquote, q {
-    quotes: none;
-  }
-
-  blockquote:before, blockquote:after,
-  q:before, q:after {
-    content: '';
-  }
-
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
-
-  * {
-    box-sizing: border-box;
-  }
-
-  body {
-    font-family: 'Pretendard-Regular', sans-serif;
-    line-height: 1.2;
-    width: 100vw;
-    height: 100vh;
-    color: #1c1c1c;
-  }
-
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-
-`;
 
 const client = new QueryClient();
 
@@ -104,7 +16,6 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
       <ThemeProvider theme={theme}>
-        <GlobalStyle />
         <RouterProvider router={router} />
       </ThemeProvider>
     </QueryClientProvider>
