d79f4d06c1a50a20aec121899c83388c20a12ab5
sorrel012 <wain1719@gmail.com>
2024-03-26 17:40:40 +0900

    [24.03.26] 프로필 페이지 컴포넌트 분리

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,20 +1,14 @@
-import styled from 'styled-components';
 import ProfileIntro from '../../components/profile/ProfileIntro';
 import ProfileMain from '../../components/profile/ProfileMain';
 import ProfileHistory from '../../components/profile/ProfileHistory';
 
-const Wrapper = styled.div`
-  background: ${(props) => props.theme.profile.bgColor};
-  height: 100vh;
-`;
-
 function Profile() {
   return (
-    <Wrapper>
+    <>
       <ProfileIntro />
       <ProfileMain />
       <ProfileHistory />
-    </Wrapper>
+    </>
   );
 }
 
