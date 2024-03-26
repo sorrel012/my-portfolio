54eee769c66fd0578ec1751aeac238c0f401952f
sorrel012 <wain1719@gmail.com>
2024-03-26 23:14:52 +0900

    [24.03.26] 프로필 intro 페이지 마크업

--- src/components/profile/ProfileIntro.tsx
+++ src/components/profile/ProfileIntro.tsx
@@ -4,51 +4,15 @@ import mushroom from '../../assets/images/home/mushroom.png';
 const Wrapper = styled.div`
   background: ${(props) => props.theme.profile.bgColor};
   height: 100vh;
-  position: relative;
-`;
-
-const WaveBox = styled.div`
-  overflow: hidden;
-  position: absolute;
-  bottom: 0;
-  margin: 0 auto;
-  max-width: 800px;
-  width: 100%;
-  height: 300px;
-  opacity: 0.5;
-`;
-
-const Wave = styled.div`
-  position: relative;
-  display: block;
-  margin: auto;
-  width: 100%;
-  height: 100%;
-
-  div {
-    background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1600' height='157'%3E%3Cdefs%3E%3ClinearGradient id='a' x1='50%25' x2='50%25' y1='-10.959%25' y2='100%25'%3E%3Cstop stop-color='%23ffffff' stop-opacity='1' offset='0%25'/%3E%3Cstop stop-color='%23ffffff' offset='100%25'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cpath fill='url(%23a)' fill-rule='evenodd' d='M.005 80C311 80 409.898-.25 811 0c400 0 500 80 789 80v77H0s.005-48 .005-77z' transform='matrix(-1 0 0 1 1600 0)'/%3E%3C/svg%3E")
-      repeat-x;
-    position: absolute;
-    bottom: 0;
-    width: 6400px;
-    height: 157px;
-    animation: wave 3s cubic-bezier(0.36, 0.45, 0.63, 0.53) infinite;
-    transform: translate3d(0, 0, 0);
-  }
-  @keyframes wave {
-    0% {
-      margin-left: -1600px;
-    }
-    100% {
-      margin-left: 0;
-    }
-  }
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Introduction = styled.div`
-  position: absolute;
-  bottom: 0;
-  padding: 0 40px;
+  height: 50vh;
+  padding: 0 5%;
   display: flex;
   color: ${(props) => props.theme.profile.titleColor};
   z-index: 99;
@@ -79,11 +43,6 @@ const Content = styled.h3`
 function ProfileIntro() {
   return (
     <Wrapper>
-      <WaveBox>
-        <Wave>
-          <div></div>
-        </Wave>
-      </WaveBox>
       <Introduction>
         <Picture src={mushroom} />
         <Overview>
