97a3eb8f450b712a98b4bb03c3b024420a06769d
sorrel012 <wain1719@gmail.com>
2024-03-29 10:27:06 +0900

    [24.03.29] Projects 페이지 마크업

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,22 +1,44 @@
 import styled from 'styled-components';
 import Project from '../components/project/Project.tsx';
 
+import turtle from '../assets/images/projects/project-turtle.png';
+import rabbit from '../assets/images/projects/project-rabbit.png';
+
 const Wrapper = styled.main`
   height: 100vh;
   width: 100vw;
   background: ${(props) => props.theme.projects.bgColor};
+  display: flex;
+  justify-content: space-around;
+  padding-top: 15%;
 `;
 
-const Turtle = styled.img``;
+const Turtle = styled.div`
+  width: 10%;
+`;
 
-const Rabbit = styled.img``;
+const TurtleImg = styled.img`
+  width: 100%;
+`;
+
+const Rabbit = styled.div`
+  width: 10%;
+`;
+
+const RabbitImg = styled.img`
+  width: 100%;
+`;
 
 function Projects() {
   return (
     <Wrapper>
-      <Turtle />
+      <Turtle>
+        <TurtleImg src={turtle} />
+      </Turtle>
       <Project />
-      <Rabbit />
+      <Rabbit>
+        <RabbitImg src={rabbit} />
+      </Rabbit>
     </Wrapper>
   );
 }
