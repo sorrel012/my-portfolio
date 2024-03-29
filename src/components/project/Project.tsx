015b14285e2e7d40d04cb03240e9bd78b64b6d94
sorrel012 <wain1719@gmail.com>
2024-03-29 10:38:28 +0900

    [24.03.29] Project 페이지 마크업

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,26 +1,49 @@
 import styled from 'styled-components';
 import ProjectItem from './ProjectItem.tsx';
 
-const Wrapper = styled.main`
-  height: 100vh;
-  width: 60vw;
-  background: ${(props) => props.theme.projects.wrapperBgColor};
+import line from '../../assets/images/projects/line.png';
+
+const Wrapper = styled.div`
+  width: 60%;
 `;
 
-const Title = styled.h2``;
+const Title = styled.div`
+  position: relative;
+  background-color: ${(props) => props.theme.projects.wrapperBgColor};
+`;
 
-const RaceImg = styled.img``;
+const RaceImg = styled.img`
+  width: 100%;
+`;
 
-const TitleHeader = styled.div``;
+const TitleHeader = styled.h2`
+  color: ${(props) => props.theme.projects.headerTextColor};
+  background-color: ${(props) => props.theme.projects.headerBgColor};
+  padding: 10px 20px;
+  border-radius: 15px;
+  font-size: 3vw;
+  font-weight: bolder;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
 
-const Content = styled.div``;
+const Content = styled.div`
+  background: ${(props) => props.theme.projects.wrapperBgColor};
+  padding: 50px 20px;
+
+  @media (max-width: 768px) {
+    padding: 30px 20px;
+  }
+`;
 
 function Project() {
   return (
     <Wrapper>
       <Title>
-        <RaceImg />
-        <TitleHeader />
+        <RaceImg src={line} />
+        <TitleHeader>PROJECTS</TitleHeader>
       </Title>
       <Content>
         <ProjectItem />
