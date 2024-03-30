deff89eab729a6688b0d394d91fb73446ca94a94
sorrel012 <wain1719@gmail.com>
2024-03-30 11:24:22 +0900

    [24.03.30] 거북이 토끼 애니메이션 구현

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,31 +1,34 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 import Project from '../components/project/Project.tsx';
 
 import turtle from '../assets/images/projects/project-turtle.png';
 import rabbit from '../assets/images/projects/project-rabbit.png';
+import { useEffect, useRef, useState } from 'react';
 
 const Wrapper = styled.main`
   height: 100vh;
   width: 100vw;
   background: ${(props) => props.theme.projects.bgColor};
   overflow-y: scroll;
-  padding: 10% 0 8%;
+  padding-top: 10%;
   position: relative;
 `;
 
-const Turtle = styled.div`
+const Turtle = styled(motion.div)`
   width: 10%;
   position: absolute;
-  top: 0;
+  left: 5%;
 `;
 
 const TurtleImg = styled.img`
   width: 100%;
 `;
 
-const Rabbit = styled.div`
+const Rabbit = styled(motion.div)`
   width: 10%;
   position: absolute;
+  right: 5%;
   top: 0;
 `;
 
@@ -34,13 +37,49 @@ const RabbitImg = styled.img`
 `;
 
 function Projects() {
+  const scrollRef = useRef<HTMLElement>(null);
+  const projectRef = useRef<HTMLDivElement>(null);
+  const turtleRef = useRef<HTMLDivElement>(null);
+  const [moveRange, setMoveRange] = useState<number>(0); // 거북이가 이동할 범위
+
+  useEffect(() => {
+    const updateMoveRange = () => {
+      if (projectRef.current && turtleRef.current) {
+        const projectHeight = projectRef.current.offsetHeight;
+        const turtleHeight = turtleRef.current.offsetHeight;
+        setMoveRange(projectHeight - turtleHeight);
+      }
+    };
+
+    window.addEventListener('resize', updateMoveRange);
+    updateMoveRange();
+
+    return () => window.removeEventListener('resize', updateMoveRange);
+  }, []);
+
+  const turtleVariants = {
+    scroll: {
+      y: [0, moveRange],
+      transition: { duration: 30 },
+    },
+  };
+
+  const rabbitVariants = {
+    scroll: {
+      y: [100, moveRange / 1.3],
+      transition: { duration: 15 },
+    },
+  };
+
   return (
-    <Wrapper>
-      <Turtle>
+    <Wrapper ref={scrollRef}>
+      <Turtle ref={turtleRef} variants={turtleVariants} animate="scroll">
         <TurtleImg src={turtle} alt="turtle" />
       </Turtle>
-      <Project />
-      <Rabbit>
+      <div ref={projectRef}>
+        <Project />
+      </div>
+      <Rabbit variants={rabbitVariants} animate="scroll">
         <RabbitImg src={rabbit} alt="rabbit" />
       </Rabbit>
     </Wrapper>
