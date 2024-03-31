bf57250ab53611e71778169b7620351ca3c46eff
sorrel012 <wain1719@gmail.com>
2024-03-31 15:35:18 +0900

    [24.03.31] 헤더 마크업

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { Outlet, useMatch } from 'react-router-dom';
+import Header from './components/Header.tsx';
 
 function App() {
-  const mainMatch = useMatch('/');
+  const homeMatch = useMatch('/');
+  const profileMatch = useMatch('/profile');
+  const skillsMatch = useMatch('/skills');
+  const projectsMatch = useMatch('/projects');
+  const contactMatch = useMatch('/contact');
+
+  const category =
+    [homeMatch, profileMatch, skillsMatch, projectsMatch, contactMatch]
+      .find((match) => match !== null)
+      ?.pathname.slice(1) || '';
 
   return (
     <>
-      {/*{!mainMatch && <Header />}*/}
+      {!homeMatch && <Header category={category} />}
       <Outlet />
     </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,106 @@
-function Header() {
-  return <h1>Header</h1>;
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+const HeaderWrapper = styled.header`
+  position: absolute;
+  top: 0;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  padding: 2%;
+  z-index: 99;
+`;
+
+const HomeButton = styled.button<IHeaderProps>`
+  border: 1px solid ${(props) => props.theme.header[props.category].borderColor};
+  border-radius: 8px;
+  background-color: ${(props) => props.theme.header[props.category].bgColor};
+  font-size: 1.8vw;
+  color: ${(props) => props.theme.header[props.category].textColor};
+  padding: 8px 20px;
+  cursor: pointer;
+`;
+
+const Category = styled.div``;
+
+const Button = styled.button<IHeaderProps>`
+  border: 1px solid ${(props) => props.theme.header[props.category].borderColor};
+  border-radius: 8px;
+  background-color: ${(props) => props.theme.header[props.category].bgColor};
+  font-size: 1.8vw;
+  color: ${(props) => props.theme.header[props.category].textColor};
+  margin-left: 20px;
+  padding: 8px 20px;
+  cursor: pointer;
+`;
+
+enum CATEGORIES {
+  HOME = 'HOME',
+  PROFILE = 'PROFILE',
+  SKILLS = 'SKILLS',
+  PROJECTS = 'PROJECTS',
+  CONTACT = 'CONTACT',
+}
+interface IHeaderProps {
+  category: 'profile' | 'skills' | 'projects' | 'contact' | 'login';
+}
+
+function Header({ category }: IHeaderProps) {
+  const navigate = useNavigate();
+
+  const onButtonClick = (category: string) => {
+    console.log(category);
+    if (category === 'HOME') {
+      navigate('/');
+    } else {
+      navigate(`/${category.toLowerCase()}`);
+    }
+  };
+
+  return (
+    <HeaderWrapper>
+      <HomeButton
+        category={category}
+        onClick={() => onButtonClick(CATEGORIES.HOME)}
+      >
+        {CATEGORIES.HOME}
+      </HomeButton>
+      <Category>
+        {category !== 'profile' && (
+          <Button
+            category={category}
+            onClick={() => onButtonClick(CATEGORIES.PROFILE)}
+          >
+            {CATEGORIES.PROFILE}
+          </Button>
+        )}
+        {category !== 'skills' && (
+          <Button
+            category={category}
+            onClick={() => onButtonClick(CATEGORIES.SKILLS)}
+          >
+            {CATEGORIES.SKILLS}
+          </Button>
+        )}
+        {category !== 'projects' && (
+          <Button
+            category={category}
+            onClick={() => onButtonClick(CATEGORIES.PROJECTS)}
+          >
+            {CATEGORIES.PROJECTS}
+          </Button>
+        )}
+        {category !== 'contact' && (
+          <Button
+            category={category}
+            onClick={() => onButtonClick(CATEGORIES.CONTACT)}
+          >
+            {CATEGORIES.CONTACT}
+          </Button>
+        )}
+      </Category>
+    </HeaderWrapper>
+  );
 }
 
 export default Header;
