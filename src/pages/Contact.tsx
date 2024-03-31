f836b0a442a766425b6d6a31d75c01d6df3e7921
sorrel012 <wain1719@gmail.com>
2024-03-31 14:01:14 +0900

    [24.03.31] Contact 페이지 밤하늘 애니메이션 추가

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -11,8 +11,8 @@ const Wrapper = styled.main`
   background: ${(props) => props.theme.contact.bgColor};
   text-align: center;
   padding-top: 10%;
-  padding-bottom: 5%;
   overflow-y: scroll;
+  position: relative;
 `;
 
 const Category = styled.h1`
@@ -28,6 +28,9 @@ const Category = styled.h1`
 const ContactWrapper = styled.section`
   display: flex;
   justify-content: center;
+  align-items: center;
+  z-index: 99;
+  position: relative;
 `;
 
 const Wing = styled.img`
@@ -40,6 +43,7 @@ const Box = styled.form`
   border-radius: 25px;
   padding: 2%;
   text-align: left;
+  z-index: 99;
 `;
 
 const Row = styled.div`
@@ -74,7 +78,7 @@ const Input = styled.input`
   padding: 5px 15px;
   width: 100%;
   border: none;
-  border-radius: 15px;
+  border-radius: 10px;
   font-size: 2vw;
   font-family: 'SUITE-Regular', sans-serif;
   &:focus-visible {
@@ -87,7 +91,7 @@ const TextArea = styled.textarea`
   padding: 5px 15px;
   width: 100%;
   border: none;
-  border-radius: 15px;
+  border-radius: 10px;
   font-size: 2vw;
   font-family: 'SUITE-Regular', sans-serif;
   &:focus-visible {
@@ -103,9 +107,60 @@ const Title = styled.div``;
 
 const Content = styled.div``;
 
-const Stars = styled.div``;
+const Bottom = styled.div`
+  height: 15%;
+  position: relative;
+`;
 
-const Star = styled(motion.img)``;
+interface IStarPosition {
+  top: string;
+  left: string;
+}
+
+const Star = styled(motion.svg)<IStarPosition>`
+  width: 1vw;
+  position: absolute;
+  top: ${(props) => props.top};
+  left: ${(props) => props.left};
+  z-index: 1;
+`;
+
+function generateTopStars(n: number): IStarPosition[] {
+  const stars: IStarPosition[] = [];
+  for (let i = 0; i < n; i++) {
+    const top = `${Math.floor(Math.random() * 30) + 5}%`;
+    const left = `${Math.floor(Math.random() * 96)}%`;
+
+    stars.push({ top, left });
+  }
+  return stars;
+}
+function generateStars(n: number): IStarPosition[] {
+  const stars: IStarPosition[] = [];
+  for (let i = 0; i < n; i++) {
+    const top = `${Math.floor(Math.random() * 95) + 5}%`;
+    const left = `${Math.floor(Math.random() * 96)}%`;
+
+    stars.push({ top, left });
+  }
+  return stars;
+}
+
+const stars: IStarPosition[] = generateTopStars(15);
+const centerStars: IStarPosition[] = generateStars(30);
+const bottomStars: IStarPosition[] = generateStars(10);
+
+const starVariants = {
+  initial: { fill: 'rgba(255, 255, 255, 0)' },
+  sparkle: {
+    fill: 'rgba(255, 255, 255, 1)',
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+      repeatType: 'reverse' as const,
+    },
+  },
+};
 
 function Contact() {
   return (
@@ -146,10 +201,57 @@ function Contact() {
           </Content>
         </Box>
         <Wing src={wing2} alt="wing" />
+        {centerStars.map((star, i) => (
+          <Star
+            key={i}
+            top={star.top}
+            left={star.left}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 576 512"
+          >
+            <motion.path
+              d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+              variants={starVariants}
+              initial="initial"
+              animate="sparkle"
+            />
+          </Star>
+        ))}
       </ContactWrapper>
-      <Stars>
-        <Star />
-      </Stars>
+      <Bottom>
+        {bottomStars.map((star, i) => (
+          <Star
+            key={i}
+            top={star.top}
+            left={star.left}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 576 512"
+          >
+            <motion.path
+              d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+              variants={starVariants}
+              initial="initial"
+              animate="sparkle"
+            />
+          </Star>
+        ))}
+      </Bottom>
+      {stars.map((star, i) => (
+        <Star
+          key={i}
+          top={star.top}
+          left={star.left}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 576 512"
+        >
+          <motion.path
+            d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+            variants={starVariants}
+            initial="initial"
+            animate="sparkle"
+          />
+        </Star>
+      ))}
     </Wrapper>
   );
 }
