29ad6055dd90bd730a5a2878046294bee75f599e
sorrel012 <wain1719@gmail.com>
2024-04-03 20:44:15 +0900

    [24.04.03] Admin 페이지 마크업

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -1,10 +1,17 @@
 import SideBar from '../../components/admin/SideBar.tsx';
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.nav`
+  display: flex;
+`;
 
 function Admin() {
   return (
-    <>
+    <Wrapper>
       <SideBar />
-    </>
+      <Outlet />
+    </Wrapper>
   );
 }
 
