e412406f074788351f9efa60d1089ec2fd3cbc5b
sorrel012 <wain1719@gmail.com>
2024-04-03 10:53:34 +0900

    [24.04.03] Login 페이지 마크업

--- src/pages/admin/Login.tsx
+++ src/pages/admin/Login.tsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
-import Header from '../../components/Header.tsx';
+import Header from '../../components/Header';
 import flower1 from '../../assets/images/home/flower1.png';
 import flower3 from '../../assets/images/home/flower3.png';
 import flower5 from '../../assets/images/home/flower5.png';
 import flower2 from '../../assets/images/home/flower2.png';
 import flower4 from '../../assets/images/home/flower4.png';
+import rabbit from '../../assets/images/admin/login-rabbit.png';
 import {
   BackgroundWrapper,
   Flower,
@@ -13,17 +14,94 @@ import {
   FlowerSecondRow,
   GRASS,
   Grass,
-} from '../Home.tsx';
+} from '../Home';
 
-const Wrapper = styled.div`
+const Wrapper = styled.main`
   height: 100vh;
   background: ${(props) => props.theme.home.bgColor};
 `;
+
+const MainWrapper = styled.section`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Book = styled.img<{ src: string }>`
+  background-image: url(${(props) => props.src});
+  background-size: contain;
+  background-position: center center;
+  width: 65%;
+  position: relative;
+`;
+
+const Form = styled.form`
+  width: 55%;
+  position: absolute;
+`;
+
+const InputBox = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const Col = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${(props) => props.theme.admin.loginColor};
+  font-size: 3vw;
+`;
+
+const LoginButton = styled.img`
+  width: 15%;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  margin-top: 5%;
+`;
+
+const BOOK = '/src/assets/images/admin/login-book.png';
 
 function Login() {
   return (
     <Wrapper>
       <Header category="login" />
+      <MainWrapper>
+        <Book src={BOOK}></Book>
+        <Form>
+          <InputBox>
+            <Col>
+              <label htmlFor="id" className="mg-b-30">
+                아이디
+              </label>
+              <label htmlFor="pw">비밀번호</label>
+            </Col>
+            <Col>
+              <input
+                id="id"
+                className="font-size-2half font-default pd-lr-10 mg-b-30
+                 "
+                style={{
+                  border: '1px solid #6D422A',
+                  borderRadius: 5,
+                  outline: 'none',
+                }}
+              />
+              <input
+                id="pw"
+                className="font-size-2half font-default pd-lr-10 "
+                style={{
+                  border: '1px solid #6D422A',
+                  borderRadius: 5,
+                  outline: 'none',
+                }}
+              />
+            </Col>
+          </InputBox>
+          <LoginButton src={rabbit} alt="rabbit" />
+        </Form>
+      </MainWrapper>
       <BackgroundWrapper>
         <Grass src={GRASS} />
         <Flowers>
